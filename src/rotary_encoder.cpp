abd01183d9fb3e87c597409005d8fb333fc0598b
ygdstmidn <2022d12@stu.gifu-nct.ac.jp>
2024-09-19 17:17:54 +0900

    🐛 last_countをメンバ変数にした

--- src/rotary_encoder.cpp
+++ src/rotary_encoder.cpp
@@ -2,171 +2,172 @@
 
 namespace mbed
 {
-namespace lib_ygdstmidn
-{
-
-mbed::lib_ygdstmidn::rotary_encoder::rotary_encoder(PinName pinA, PinName pinB, PinName pinZ)
-{
-    read_encode_z  =new DigitalIn(pinZ);
-    wa_encode_pinA  =new InterruptIn(pinA);
-    wa_encode_pinB  =new InterruptIn(pinB);
-
-    read_encode_z->mode(ROTARY_ENCODER_DEFAULT_PIN_Z_PINMODE);
-
-    wa_encode_pinA->rise(callback(this, &rotary_encoder::encode));//割り込み
-    wa_encode_pinA->fall(callback(this, &rotary_encoder::encode));
-    wa_encode_pinA->mode(ROTARY_ENCODER_DEFAULT_PIN_A_PINMODE);
-
-    wa_encode_pinB->rise(callback(this, &rotary_encoder::encode));
-    wa_encode_pinB->fall(callback(this, &rotary_encoder::encode));
-    wa_encode_pinB->mode(ROTARY_ENCODER_DEFAULT_PIN_B_PINMODE);
-}
-
-mbed::lib_ygdstmidn::rotary_encoder::rotary_encoder(PinName pinA, PinName pinB)
-{
-    read_encode_z  =nullptr;
-    wa_encode_pinA  =new InterruptIn(pinA);
-    wa_encode_pinB  =new InterruptIn(pinB);
-
-    // read_encode_z->mode(ROTARY_ENCODER_DEFAULT_PIN_Z_PINMODE);
-
-    wa_encode_pinA->rise(callback(this, &rotary_encoder::encode));//割り込み
-    wa_encode_pinA->fall(callback(this, &rotary_encoder::encode));
-    wa_encode_pinA->mode(ROTARY_ENCODER_DEFAULT_PIN_A_PINMODE);
-
-    wa_encode_pinB->rise(callback(this, &rotary_encoder::encode));
-    wa_encode_pinB->fall(callback(this, &rotary_encoder::encode));
-    wa_encode_pinB->mode(ROTARY_ENCODER_DEFAULT_PIN_B_PINMODE);
-}
-
-mbed::lib_ygdstmidn::rotary_encoder::~rotary_encoder()
-{
-    delete read_encode_z;
-    delete wa_encode_pinA;
-    delete wa_encode_pinB;
-}
-
-void mbed::lib_ygdstmidn::rotary_encoder::encode()
-{
-    static int last_state1 = 0;
-    int sig1_A = *wa_encode_pinA;
-    int sig1_B = *wa_encode_pinB;
-    int8_t state1 = sig1_A | (sig1_B << 1);
-    int8_t sum1 = state1 | last_state1<<2;
-    if (last_state1 != state1)
+    namespace lib_ygdstmidn
     {
-        encode_count+=encode_table[sum1];
-        last_state1=state1;
-    }
-}
 
-int mbed::lib_ygdstmidn::rotary_encoder::get_count()const
-{
-    return encode_count;
-}
+        mbed::lib_ygdstmidn::rotary_encoder::rotary_encoder(PinName pinA, PinName pinB, PinName pinZ)
+        {
+            read_encode_z = new DigitalIn(pinZ);
+            wa_encode_pinA = new InterruptIn(pinA);
+            wa_encode_pinB = new InterruptIn(pinB);
 
-int mbed::lib_ygdstmidn::rotary_encoder::get_count_diff()const
-{
-    static int last_count=0;
-    int diff=encode_count-last_count;
-    last_count=encode_count;
-    return diff;
-}
+            read_encode_z->mode(ROTARY_ENCODER_DEFAULT_PIN_Z_PINMODE);
 
-void mbed::lib_ygdstmidn::rotary_encoder::set_pinmode(PinMode pinA, PinMode pinB, PinMode pinZ)
-{
-    wa_encode_pinA->mode(pinA);
-    wa_encode_pinB->mode(pinB);
-    if (read_encode_z!=nullptr)
-    {
-        read_encode_z->mode(pinZ);
-    }
-}
+            wa_encode_pinA->rise(callback(this, &rotary_encoder::encode)); // 割り込み
+            wa_encode_pinA->fall(callback(this, &rotary_encoder::encode));
+            wa_encode_pinA->mode(ROTARY_ENCODER_DEFAULT_PIN_A_PINMODE);
 
-void rotary_encoder::set_ppr(uint set_ppr_uint)
-{
-    ppr=set_ppr_uint;
-}
+            wa_encode_pinB->rise(callback(this, &rotary_encoder::encode));
+            wa_encode_pinB->fall(callback(this, &rotary_encoder::encode));
+            wa_encode_pinB->mode(ROTARY_ENCODER_DEFAULT_PIN_B_PINMODE);
+        }
 
-double rotary_encoder::get_angle()const
-{
-    return (double)encode_count*360.0/ppr;
-}
+        mbed::lib_ygdstmidn::rotary_encoder::rotary_encoder(PinName pinA, PinName pinB)
+        {
+            read_encode_z = nullptr;
+            wa_encode_pinA = new InterruptIn(pinA);
+            wa_encode_pinB = new InterruptIn(pinB);
 
-double rotary_encoder::get_angle_radian()const
-{
-    return (double)encode_count/ppr*2.0*M_PI;
-}
+            // read_encode_z->mode(ROTARY_ENCODER_DEFAULT_PIN_Z_PINMODE);
 
-void rotary_encoder::set_teeth(uint set_teeth_uint)
-{
-    if(set_teeth_uint!=0)
-    {
-        gear_teeth=set_teeth_uint;
-    }
-}
+            wa_encode_pinA->rise(callback(this, &rotary_encoder::encode)); // 割り込み
+            wa_encode_pinA->fall(callback(this, &rotary_encoder::encode));
+            wa_encode_pinA->mode(ROTARY_ENCODER_DEFAULT_PIN_A_PINMODE);
 
-uint rotary_encoder::get_teeth()const
-{
-    return gear_teeth;
-}
+            wa_encode_pinB->rise(callback(this, &rotary_encoder::encode));
+            wa_encode_pinB->fall(callback(this, &rotary_encoder::encode));
+            wa_encode_pinB->mode(ROTARY_ENCODER_DEFAULT_PIN_B_PINMODE);
+        }
 
-//以下，rotary_encoder_gearクラス↓↓↓
+        mbed::lib_ygdstmidn::rotary_encoder::~rotary_encoder()
+        {
+            delete read_encode_z;
+            delete wa_encode_pinA;
+            delete wa_encode_pinB;
+        }
 
-rotary_encoder_gear::rotary_encoder_gear(rotary_encoder *rotary_encoder_address)
-{
-    rotary_encoder_ins=rotary_encoder_address;
-    gear_ins=nullptr;
+        void mbed::lib_ygdstmidn::rotary_encoder::encode()
+        {
+            static int last_state1 = 0;
+            int sig1_A = *wa_encode_pinA;
+            int sig1_B = *wa_encode_pinB;
+            int8_t state1 = sig1_A | (sig1_B << 1);
+            int8_t sum1 = state1 | last_state1 << 2;
+            if (last_state1 != state1)
+            {
+                encode_count += encode_table[sum1];
+                last_state1 = state1;
+            }
+        }
 
-    rotary_encoder_teeth=rotary_encoder_ins->get_teeth();
-}
+        int mbed::lib_ygdstmidn::rotary_encoder::get_count() const
+        {
+            return encode_count;
+        }
 
-rotary_encoder_gear::rotary_encoder_gear(rotary_encoder_gear *rotary_encoder_gear_address)
-{
-    rotary_encoder_ins=nullptr;
-    gear_ins=rotary_encoder_gear_address;
+        int mbed::lib_ygdstmidn::rotary_encoder::get_count_diff()
+        {
+            int diff = encode_count - last_count;
+            last_count = encode_count;
+            return diff;
+        }
 
-    rotary_encoder_teeth=gear_ins->get_teeth();
-}
+        void mbed::lib_ygdstmidn::rotary_encoder::set_pinmode(PinMode pinA, PinMode pinB, PinMode pinZ)
+        {
+            wa_encode_pinA->mode(pinA);
+            wa_encode_pinB->mode(pinB);
+            if (read_encode_z != nullptr)
+            {
+                read_encode_z->mode(pinZ);
+            }
+        }
 
-rotary_encoder_gear::~rotary_encoder_gear()
-{
-}
+        void rotary_encoder::set_ppr(uint set_ppr_uint)
+        {
+            ppr = set_ppr_uint;
+        }
 
-void rotary_encoder_gear::set_teeth(uint set_teeth_uint)
-{
-    if(set_teeth_uint!=0)
-    {
-        gear_teeth=set_teeth_uint;
-    }
-}
+        double rotary_encoder::get_angle() const
+        {
+            return (double)encode_count * 360.0 / ppr;
+        }
 
-uint rotary_encoder_gear::get_teeth()const
-{
-    return gear_teeth;
-}
+        double rotary_encoder::get_angle_radian() const
+        {
+            return (double)encode_count / ppr * 2.0 * M_PI;
+        }
 
-double rotary_encoder_gear::get_angle() const
-{
-    if(rotary_encoder_ins==nullptr)
-    {
-        return (gear_ins->get_angle())*(gear_ins->get_teeth())/(gear_teeth);
-    }else
-    {
-        return (rotary_encoder_ins->get_angle())*(rotary_encoder_ins->get_teeth())/(gear_teeth);
-    }
-}
+        void rotary_encoder::set_teeth(uint set_teeth_uint)
+        {
+            if (set_teeth_uint != 0)
+            {
+                gear_teeth = set_teeth_uint;
+            }
+        }
 
-double rotary_encoder_gear::get_angle_radian() const
-{
-    if(rotary_encoder_ins==nullptr)
-    {
-        return (gear_ins->get_angle_radian())*(gear_ins->get_teeth())/(gear_teeth);
-    }else
-    {
-        return (rotary_encoder_ins->get_angle_radian())*(rotary_encoder_ins->get_teeth())/(gear_teeth);
-    }
-}
+        uint rotary_encoder::get_teeth() const
+        {
+            return gear_teeth;
+        }
 
-} // namespace lib_ygdstmidn
-}//namespace mbed
+        // 以下，rotary_encoder_gearクラス↓↓↓
+
+        rotary_encoder_gear::rotary_encoder_gear(rotary_encoder *rotary_encoder_address)
+        {
+            rotary_encoder_ins = rotary_encoder_address;
+            gear_ins = nullptr;
+
+            rotary_encoder_teeth = rotary_encoder_ins->get_teeth();
+        }
+
+        rotary_encoder_gear::rotary_encoder_gear(rotary_encoder_gear *rotary_encoder_gear_address)
+        {
+            rotary_encoder_ins = nullptr;
+            gear_ins = rotary_encoder_gear_address;
+
+            rotary_encoder_teeth = gear_ins->get_teeth();
+        }
+
+        rotary_encoder_gear::~rotary_encoder_gear()
+        {
+        }
+
+        void rotary_encoder_gear::set_teeth(uint set_teeth_uint)
+        {
+            if (set_teeth_uint != 0)
+            {
+                gear_teeth = set_teeth_uint;
+            }
+        }
+
+        uint rotary_encoder_gear::get_teeth() const
+        {
+            return gear_teeth;
+        }
+
+        double rotary_encoder_gear::get_angle() const
+        {
+            if (rotary_encoder_ins == nullptr)
+            {
+                return (gear_ins->get_angle()) * (gear_ins->get_teeth()) / (gear_teeth);
+            }
+            else
+            {
+                return (rotary_encoder_ins->get_angle()) * (rotary_encoder_ins->get_teeth()) / (gear_teeth);
+            }
+        }
+
+        double rotary_encoder_gear::get_angle_radian() const
+        {
+            if (rotary_encoder_ins == nullptr)
+            {
+                return (gear_ins->get_angle_radian()) * (gear_ins->get_teeth()) / (gear_teeth);
+            }
+            else
+            {
+                return (rotary_encoder_ins->get_angle_radian()) * (rotary_encoder_ins->get_teeth()) / (gear_teeth);
+            }
+        }
+
+    } // namespace lib_ygdstmidn
+} // namespace mbed
--- src/rotary_encoder.h
+++ src/rotary_encoder.h
@@ -5,158 +5,155 @@
 #include "mbed.h"
 #include "rotary_encoder_config.h"
 
-#ifndef M_PI//M_PIが定義されていないならば定義
-#define M_PI		3.14159265358979323846
-#endif//M_PI
-#ifndef _SYS_TYPES_H//uintが定義されていないならば定義
-typedef	unsigned int	uint;		/* System V compatibility */
-#endif//_SYS_TYPES_H
+#ifndef M_PI // M_PIが定義されていないならば定義
+#define M_PI 3.14159265358979323846
+#endif                     // M_PI
+#ifndef _SYS_TYPES_H       // uintが定義されていないならば定義
+typedef unsigned int uint; /* System V compatibility */
+#endif                     //_SYS_TYPES_H
 
 namespace mbed
 {
-namespace lib_ygdstmidn
-{
+    namespace lib_ygdstmidn
+    {
 
-const int encode_table[16] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
+        const int encode_table[16] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
 
-class rotary_encoder
-{
-private:
+        class rotary_encoder
+        {
+        private:
+            int encode_count = 0;
+            uint gear_teeth = 1;
+            uint ppr = ROTARY_ENCODER_DEFAULT_PPR;
+            int last_count = 0;
 
-    int encode_count=0;
-    uint gear_teeth=1;
-    uint ppr=ROTARY_ENCODER_DEFAULT_PPR;
+            DigitalIn *read_encode_z;
 
-    DigitalIn *read_encode_z;
+            InterruptIn *wa_encode_pinA;
+            InterruptIn *wa_encode_pinB;
 
-    InterruptIn *wa_encode_pinA;
-    InterruptIn *wa_encode_pinB;
+            void encode();
 
-    void encode();
+        public:
+            /**
+             * ロータリーエンコーダの値を読み取るためのインスタンスを作成します
+             * @param pinA ロータリーエンコーダのAチャンネル
+             * @param pinB ロータリーエンコーダのBチャンネル
+             * @param pinZ ロータリーエンコーダのZチャンネル,INDEXチャンネル
+             */
+            rotary_encoder(PinName pinA, PinName pinB, PinName pinZ);
 
-public:
+            /**
+             * ロータリーエンコーダの値を読み取るためのインスタンスを作成します
+             * @param pinA ロータリーエンコーダのAチャンネル
+             * @param pinB ロータリーエンコーダのBチャンネル
+             */
+            rotary_encoder(PinName pinA, PinName pinB);
 
-    /**
-     * ロータリーエンコーダの値を読み取るためのインスタンスを作成します
-     * @param pinA ロータリーエンコーダのAチャンネル
-     * @param pinB ロータリーエンコーダのBチャンネル
-     * @param pinZ ロータリーエンコーダのZチャンネル,INDEXチャンネル
-    */
-    rotary_encoder(PinName pinA,PinName pinB,PinName pinZ);
+            ~rotary_encoder();
 
-    /**
-     * ロータリーエンコーダの値を読み取るためのインスタンスを作成します
-     * @param pinA ロータリーエンコーダのAチャンネル
-     * @param pinB ロータリーエンコーダのBチャンネル
-    */
-    rotary_encoder(PinName pinA,PinName pinB);
+            /**
+             * ロータリーエンコーダの変位を返します．
+             * @return ロータリーエンコーダの変位(int)
+             */
+            int get_count() const;
 
-    ~rotary_encoder();
+            int get_count_diff();
 
-    /**
-     * ロータリーエンコーダの変位を返します．
-     * @return ロータリーエンコーダの変位(int)
-    */
-    int get_count()const;
+            /**
+             * それぞれのピンごとにmodeを設定します．
+             * PullUp, PullDown, PullNone, OpenDrain
+             * @param pinA Aチャンネルのmode
+             * @param pinB Bチャンネルのmode
+             * @param pinZ Zチャンネルのmode
+             * @note Zチャンネルを設定していない場合，modeは設定されません．
+             */
+            void set_pinmode(PinMode pinA, PinMode pinB, PinMode pinZ);
 
-    int get_count_diff()const;
+            /**
+             * ロータリーエンコーダのPPRを設定します．
+             * @param set_ppr_uint 設定したいPPR
+             */
+            void set_ppr(uint set_ppr_uint);
 
-    /**
-     * それぞれのピンごとにmodeを設定します．
-     * PullUp, PullDown, PullNone, OpenDrain
-     * @param pinA Aチャンネルのmode
-     * @param pinB Bチャンネルのmode
-     * @param pinZ Zチャンネルのmode
-     * @note Zチャンネルを設定していない場合，modeは設定されません．
-    */
-    void set_pinmode(PinMode pinA,PinMode pinB,PinMode pinZ);
+            /**
+             * ロータリーエンコーダの角度を返します(度数法)．
+             * @return ロータリーエンコーダの角度(double)
+             */
+            double get_angle() const;
 
-    /**
-     * ロータリーエンコーダのPPRを設定します．
-     * @param set_ppr_uint 設定したいPPR
-    */
-    void set_ppr(uint set_ppr_uint);
+            /**
+             * ロータリーエンコーダの角度を返します(弧度法)．
+             * @return ロータリーエンコーダの角度(double)
+             */
+            double get_angle_radian() const;
 
-    /**
-     * ロータリーエンコーダの角度を返します(度数法)．
-     * @return ロータリーエンコーダの角度(double)
-    */
-    double get_angle()const;
+            /**
+             * ロータリーエンコーダについているギアの歯数を設定します．
+             * @param set_teeth_uint 設定したい歯数
+             */
+            void set_teeth(uint set_teeth_uint);
 
-    /**
-     * ロータリーエンコーダの角度を返します(弧度法)．
-     * @return ロータリーエンコーダの角度(double)
-    */
-    double get_angle_radian()const;
+            /**
+             * 設定されているギアの歯数を返します．
+             * @return 設定されているギアの歯数
+             */
+            uint get_teeth() const;
 
-    /**
-     * ロータリーエンコーダについているギアの歯数を設定します．
-     * @param set_teeth_uint 設定したい歯数
-    */
-    void set_teeth(uint set_teeth_uint);
+        }; // class rotary_encoder
 
-    /**
-     * 設定されているギアの歯数を返します．
-     * @return 設定されているギアの歯数
-    */
-    uint get_teeth()const;
+        class rotary_encoder_gear
+        {
+        private:
+            uint rotary_encoder_teeth;
+            uint gear_teeth = 1;
 
-};//class rotary_encoder
+            rotary_encoder *rotary_encoder_ins;
+            rotary_encoder_gear *gear_ins;
 
-class rotary_encoder_gear
-{
-private:
+        public:
+            /**
+             * ロータリーエンコーダにくっつけるギアのインスタンスを作成します．
+             * @param rotary_encoder_address くっつけるロータリーエンコーダのアドレス
+             */
+            rotary_encoder_gear(rotary_encoder *rotary_encoder_address);
 
-    uint rotary_encoder_teeth;
-    uint gear_teeth=1;
+            /**
+             * ギアにくっつけるギアのインスタンスを作成します．
+             * @param rotary_encoder_gear_address くっつけるギアのアドレス
+             */
+            rotary_encoder_gear(rotary_encoder_gear *rotary_encoder_gear_address);
 
-    rotary_encoder *rotary_encoder_ins;
-    rotary_encoder_gear *gear_ins;
+            ~rotary_encoder_gear();
 
-public:
+            /**
+             * ギアの歯数を設定します．
+             * @param set_teeth_uint 設定したい歯数
+             */
+            void set_teeth(uint set_teeth_uint);
 
-    /**
-     * ロータリーエンコーダにくっつけるギアのインスタンスを作成します．
-     * @param rotary_encoder_address くっつけるロータリーエンコーダのアドレス
-    */
-    rotary_encoder_gear(rotary_encoder *rotary_encoder_address);
+            /**
+             * 設定されているギアの歯数を返します．
+             * @return 設定されているギアの歯数
+             */
+            uint get_teeth() const;
 
-    /**
-     * ギアにくっつけるギアのインスタンスを作成します．
-     * @param rotary_encoder_gear_address くっつけるギアのアドレス
-    */
-    rotary_encoder_gear(rotary_encoder_gear *rotary_encoder_gear_address);
+            /**
+             * ギアの角度を返します(度数法)．
+             * @return ギアの角度(double)
+             */
+            double get_angle() const;
 
-    ~rotary_encoder_gear();
+            /**
+             * ギアの角度を返します(弧度法)．
+             * @return ギアの角度(double)
+             */
+            double get_angle_radian() const;
+        };
 
-    /**
-     * ギアの歯数を設定します．
-     * @param set_teeth_uint 設定したい歯数
-    */
-    void set_teeth(uint set_teeth_uint);
-
-    /**
-     * 設定されているギアの歯数を返します．
-     * @return 設定されているギアの歯数
-    */
-    uint get_teeth()const;
-
-    /**
-     * ギアの角度を返します(度数法)．
-     * @return ギアの角度(double)
-    */
-    double get_angle()const;
-
-    /**
-     * ギアの角度を返します(弧度法)．
-     * @return ギアの角度(double)
-    */
-    double get_angle_radian()const;
-};
-
-}//namespace lib_ygdstmidn
-}//namespace mbed
+    } // namespace lib_ygdstmidn
+} // namespace mbed
 
 using namespace mbed::lib_ygdstmidn;
 
-#endif//INCLUDE_ROTARY_ENCODER_H
+#endif // INCLUDE_ROTARY_ENCODER_H
